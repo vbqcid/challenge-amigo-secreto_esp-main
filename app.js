881854f9f363df39c6708a2bb0dfc8e66cdfbf7e
vbqcid <vbqcid@gmail.com>
2025-01-18 12:21:21 -0500

    Agregando codigo para juego amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const listaAmigos = [];
+
+
+const inputAmigo = document.getElementById("amigo");
+const listaAmigosElemento = document.getElementById("listaAmigos");
+const resultadoElemento = document.getElementById("resultado");
+
+
+function agregarAmigo() {
+  const nombre = inputAmigo.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, ingresa un nombre válido.");
+    return;
+  }
+
+
+  listaAmigos.push(nombre);
+  inputAmigo.value = "";
+
+
+  actualizarListaAmigos();
+}
+
+
+function actualizarListaAmigos() {
+
+  listaAmigosElemento.innerHTML = "";
+
+ 
+  listaAmigos.forEach((amigo, index) => {
+    const li = document.createElement("li");
+    li.textContent = `${index + 1}. ${amigo}`;
+    li.classList.add("list-item");
+    listaAmigosElemento.appendChild(li);
+  });
+}
+
+
+function sortearAmigo() {
+  if (listaAmigos.length === 0) {
+    alert("La lista está vacía. Agrega nombres antes de sortear.");
+    return;
+  }
+
+
+  const indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+  const amigoSeleccionado = listaAmigos[indiceAleatorio];
+
+
+  resultadoElemento.innerHTML = `
+    <li class="winner-item">🎉 El amigo secreto es: <strong>${amigoSeleccionado}</strong> 🎉</li>
+  `;
+}
